bdcf8d193bb4f88c3818e32c62f702269b97c948
j22219ss庄司真吾 <j22219ss@edu.tuis.ac.jp>
2025-04-26 13:44:09 +0900

    Update dice.py
    平均値の計算と合計のsumを追加。
    平均値の書き出しを追加。

--- dice.py
+++ dice.py
@@ -1,20 +1,28 @@
 import random
 
+sum = 0 
+
 for i in range(10):
-    x = random.randint(1,6)
-    print(str(i+1)+"回目："+str(x))
+    x = random.randint(1, 6)
+    sum += x  
+    print(str(i + 1) + "回目：" + str(x))
+
+average = total / 10 
+print("平均：" + str(round(average, 1)))  
+
 
 # 期待される出力結果例
 """
-1回目：4
-2回目：1
-3回目：1
+
+1回目：3
+2回目：6
+3回目：2
 4回目：5
-5回目：1
+5回目：4
 6回目：1
-7回目：5
-8回目：6
-9回目：4
-10回目：6
-平均値：3.4
+7回目：6
+8回目：3
+9回目：2
+10回目：4
+平均：3.6
 """
